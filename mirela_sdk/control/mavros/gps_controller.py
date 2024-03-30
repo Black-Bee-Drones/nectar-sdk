de3bb393415a3a1193ba73ab7e8cb4ce93a9db28
Eduardo Castro <americogomes1@outlook.com>
2024-03-30 12:25:58 -0300

    tf_transformation adjust

--- mirela_sdk/control/mavros/gps_controller.py
+++ mirela_sdk/control/mavros/gps_controller.py
@@ -7,7 +7,7 @@ from shapely.geometry import Point, Polygon
 from geopy.distance import geodesic
 from math import radians
 
-from tf.transformations import quaternion_from_euler
+from tf_transformations import quaternion_from_euler
 from geographic_msgs.msg import GeoPoseStamped
 
 from mirela_sdk.control.mavros.mavros_api import MavDrone
