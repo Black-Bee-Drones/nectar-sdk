af558049cb9bab04a163464f38a3e9d0e6242b94
samuellimabraz <samuellimabraz@gmail.com>
2024-08-04 19:09:56 -0300

    doc: docstrings for Controller class

--- mirela_sdk/mirela_sdk/control/pid/controller.py
+++ mirela_sdk/mirela_sdk/control/pid/controller.py
@@ -9,6 +9,10 @@ from mirela_sdk.utils.process import ProcessUtils
 
 
 class Controller:
+    """
+    A class to create a Python interface for the ROS2 PID control library, enabling the configuration
+    and initialization of nodes, and management of topics and parameters programmatically.
+    """
 
     def __init__(
         self,
@@ -34,12 +38,30 @@ class Controller:
         loop_freq: int = 10,
     ) -> None:
         """
-        Inicializa o controlador com parâmetros e configurações.
+        Constructor for the Controller class.
 
-        :param name: Nome do nó do controlador.
-        :param kp: Ganho proporcional.
-        :param ki: Ganho integral.
-        :param kd: Ganho derivativo.
+        ---
+
+        :param node: The ROS2 node to be used by the controller.
+        :param name: The name of the controller node.
+        :param kp: Proportional gain.
+        :param ki: Integral gain.
+        :param kd: Derivative gain.
+        :param setpoint: Desired setpoint for the controller.
+        :param callback: Optional callback function for control value updates.
+        :param use_sample_time: Boolean to use a sample time. default: False
+        :param sample_time: Sample time for the PID controller. default: 5
+        :param derivative_on_measurement: Boolean to apply derivative on measurement. default: False
+        :param remove_ki_bump: Boolean to remove the integral bump. default: False
+        :param reset_windup: Boolean to reset windup. default: True
+        :param pid_enabled: Boolean to enable the PID controller. default: True
+        :param cut_off_freq: Cut-off frequency for the controller. default: 0
+        :param out_min: Minimum output value. default: -1
+        :param out_max: Maximum output value. default: 1
+        :param control_value_topic: Topic name for the control value. default: "control_value"
+        :param actual_state_topic: Topic name for the actual state. default: "actual_state"
+        :param set_point_topic: Topic name for the setpoint. default: "set_point"
+        :param loop_freq: Loop frequency for the controller. default: 10
         """
         self.node, self.name = node, name
         self.callback = callback
@@ -76,23 +98,24 @@ class Controller:
 
         self._init_client_params()
 
-        self.control_effort = 0.0
-        self.setpoint = setpoint
-        self.able = False
+        self.control_effort: float = 0.0
+        self.setpoint: float = setpoint
+        self.able: bool = False
 
     def _init_node(self):
         """
-        Inicializa o nó do controlador com os parâmetros especificados.
+        Initializes the controller node with specified parameters.
         """
 
         args = " -p ".join([f"{k}:={v}" for k, v in self.params.items()])
         cli = f"ros2 run use_library use_library --ros-args -p {args}"
-        ProcessUtils.start_process(cli)
+        ProcessUtils.start_process(cli, self.name)
 
     def _init_topics(self):
         """
-        Inicializa os tópicos de publicação e assinatura para o controlador.
+        Initializes the topics for publishing and subscribing to control data.
         """
+
         self.state_msg = Float32()
         self.setpoint_msg = Float32()
         self.state_pub = self.node.create_publisher(Float32, self.topics[1], 10)
@@ -118,6 +141,10 @@ class Controller:
         )
 
     def _init_client_params(self):
+        """
+        Initializes the client parameters for the ROS2 PID library.
+        """
+
         self.cli = self.node.create_client(
             SetParameters, "ros2_pid_library/set_parameters"
         )
@@ -127,7 +154,9 @@ class Controller:
 
     def _control_callback(self, msg: Float32):
         """
-        Callback para o tópico de controle.
+        Callback function for the control value topic.
+
+        :param msg: The message containing the control effort data.
         """
 
         self.control_effort = msg.data
@@ -136,7 +165,9 @@ class Controller:
 
     def enable(self, enable: bool):
         """
-        Habilita ou desabilita o controlador.
+        Enables or disables the PID controller.
+
+        :param enable: Boolean to enable or disable the controller.
         """
 
         self.able = enable
@@ -156,22 +187,38 @@ class Controller:
                 self.node.get_logger().error(f"Service call failed: {e}")
 
     @property
-    def setpoint(self):
+    def setpoint(self) -> float:
+        """
+        The setpoint property getter.
+        """
         return self._setpoint
 
     @setpoint.setter
-    def setpoint(self, value):
+    def setpoint(self, value: float):
+        """
+        The setpoint property setter.
+
+        :param value (float): The new setpoint value.
+        """
         print("setter")
         self._setpoint = value
         self.setpoint_msg.data = self._setpoint
         self.setpoint_pub.publish(self.setpoint_msg)
 
     @property
-    def state(self):
+    def state(self) -> float:
+        """
+        The state property getter.
+        """
         return self._state
 
     @state.setter
-    def state(self, value):
+    def state(self, value: float):
+        """
+        The state property setter.
+
+        :param value: The new state value.
+        """
         self.setpoint_msg.data = self._setpoint
         self.setpoint_pub.publish(self.setpoint_msg)
 
