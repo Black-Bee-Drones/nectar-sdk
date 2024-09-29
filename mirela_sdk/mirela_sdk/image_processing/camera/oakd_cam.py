f2b61058e573ffde3492e73345f4bb7ac08f1a4e
Eduard0Castro <americogomes1@outlook.com>
2024-09-29 13:21:43 -0300

    feat: stereo and imu settings for oakd_cam class and disparity implementation example

--- mirela_sdk/mirela_sdk/image_processing/camera/oakd_cam.py
+++ mirela_sdk/mirela_sdk/image_processing/camera/oakd_cam.py
@@ -4,12 +4,12 @@ import cv2
 class OakdCam:
 
     """
-    Class to initialize the pipeline and define the paramters for accessing OAKD images
+    Class to initialize the pipeline and define the parameters for accessing OAKD images
     """
     
     def __init__(self)-> None:
         """
-        OakdCam constructor: initializes the pipeline and configures cameras and their 
+        OakdCam constructor: initializes the pipeline and configures cameras with their 
         board sockets
         """
         self.pipeline = dai.Pipeline()
@@ -19,24 +19,33 @@ class OakdCam:
         self.device = None
         
 
-    def setup_camera(self, cam_num: int)-> None:
+    def setup_camera(self, 
+                     cam_num: int, 
+                     link_out: bool = True) -> dai.node.ColorCamera | dai.node.MonoCamera | None:
         """
         Function to set the camera type based on the camera number parameter
 
-        :param cam_num (int): index number for oakd cam - 1 for rgb, 2 for left monochrome cam, 3 for
-                              right monochrome cam 
+        :param cam_num (int): index number for oakd cam - 1 for rgb, 2 for left monochrome cam, 
+         3 for right monochrome cam 
+        :param link_out (bool): link the camera output with xout input, 
+         False to wait for another application
         """
         
+        self.link_out = link_out
         self.cam_num = cam_num
         self.cam_type, self.boardSocket = self.oak_dict.get(self.cam_num, ("invalid", None))
+
+        camera = None
         
         if self.cam_type != "invalid":
 
             if self.cam_type == "rgb":
-                self.color_camera()
+                camera = self.color_camera()
 
             else:
-                self.mono_camera()
+                camera = self.mono_camera()
+
+        return camera
 
 
     def init_cam(self, full_speed: bool = False) -> dai.Device:
@@ -50,6 +59,7 @@ class OakdCam:
 
         return self.device
     
+    
     def color_camera(self) -> dai.node.ColorCamera:
         """
         Link device to host, set resolution and preview size to get the color camera
@@ -60,10 +70,12 @@ class OakdCam:
         #seleciono a câmera rgb:
         cam.setBoardSocket(self.boardSocket)
         #setar resolução:
-        cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
+        cam.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
         #setar tamanho
         cam.setPreviewSize(960, 540)
-
+        #setar fps
+        cam.setFps(30)
+        
         #criar link de comunicação da camera com o host(pc)
         xOut_rgb = self.pipeline.createXLinkOut()
         xOut_rgb.setStreamName("rgb")
@@ -72,6 +84,7 @@ class OakdCam:
         cam.preview.link(xOut_rgb.input)
 
         return cam
+    
     
     def mono_camera(self) -> dai.node.MonoCamera:
         """
@@ -84,7 +97,8 @@ class OakdCam:
         cam.setBoardSocket(self.boardSocket)
         xout = self.pipeline.createXLinkOut()
         xout.setStreamName(self.cam_type)
-        cam.out.link(xout.input)
+
+        if self.link_out: cam.out.link(xout.input)
 
         return cam
     
@@ -96,21 +110,112 @@ class OakdCam:
         
         return self.device.getOutputQueue(self.cam_type)
     
-    def getQueue(self, stream_name: str) -> dai.DataOutputQueue:
+    
+    def getQueue(self, 
+                 stream_name: str, 
+                 maxSize: int, 
+                 blocking: bool = True) -> dai.DataOutputQueue:
         """
         Gets an output queue corresponding to stream name. If it doesn't exist it throws
+        param stream_name (str): the stream name for output queue
+        param maxSize (int): max size of the queue
+        param blocking (bool): True for block the return of the function until 
+        arrive new msgs in the queue. False for otherwise
         """
 
-        return self.device.getOutputQueue(stream_name)
+        return self.device.getOutputQueue(stream_name, maxSize, blocking = blocking)
         
 
     def getFrame(self, queue: dai.DataOutputQueue) -> cv2.Mat:
         """
         Gets the cv frame from output queue wich is depthai.ImgFrame
+        param queue (dai.DataOutputQueue): the output queue from getQueue function
         """
 
         return queue.get().getCvFrame()
     
+    
+    def get_stereo_depth(self) -> dai.node.StereoDepth:
+
+        """
+        Set stereo settings and get it to use. 
+        """
+
+        stereo = self.pipeline.createStereoDepth()
+        stereo.setLeftRightCheck(True)
+
+        self.stereo_links = {"disparity": stereo.disparity.link, 
+                             "depth": stereo.depth.link, 
+                             "rectifiedLeft": stereo.rectifiedLeft.link, 
+                             "rectifiedRight": stereo.rectifiedRight.link, 
+                             "syncedLeft": stereo.syncedLeft.link, 
+                             "syncedRight": stereo.syncedRight.link}
+
+        mono_left = self.setup_camera(2, False)
+        mono_right = self.setup_camera(3, False)
+
+        mono_left.out.link(stereo.left)
+        mono_right.out.link(stereo.right)
+
+        return stereo
+        
+
+    def configure_stereo_node_output(self, stream_names: list[str]) -> None:
+
+        """
+        Set the stereo output requested
+
+        param stream_names list[str]: stream names to configure
+        ("disparity", "depth", "rectifiedLeft", "rectifiedRight", "syncedLeft", "syncedRight")
+        """
+        
+        for stream_name in stream_names:
+        
+            link = self.stereo_links.get(stream_name, None)
+
+            if link:
+
+                xout = self.pipeline.createXLinkOut()
+                xout.setStreamName(stream_name)
+                link(xout.input)
+
+    def create_imu(self) -> dai.node.IMU:
+
+        """
+        Create an IMU sensor and return it.
+
+        Note: OAK-D-LITE doesn't have IMU
+        """
+
+        self.imu = self.pipeline.createIMU()
+        xout = self.pipeline.createXLinkOut()
+        xout.setStreamName("imu")
+
+        self.imu_sensors = {"accelerometer": dai.IMUSensor.ACCELEROMETER, 
+                            "gyroscope": dai.IMUSensor.GYROSCOPE_RAW}
+        
+        self.imu.out.link(xout.input)
+
+        return self.imu
+    
+
+    def enable_imu_sensor(self, sensor_name: str, rate: int) -> None:
+
+        """
+        Enable the IMU sensor requested
+        
+        param sensor_name (str): the sensor name to enable ("accelerometer", "gyroscope")
+        param rate (int): the measurement frequency, in Hz, of the sensor. Max frequencies: 
+        accelerometer -> 512 Hz, gyroscope -> 1000 Hz
+        """
+
+        sensor = self.imu_sensors.get(sensor_name, (None, None))
+        
+        if sensor:
+
+            self.imu.enableIMUSensor(sensor, rate)
+
+
     def clean(self):
         """
         Closes the connection to device
