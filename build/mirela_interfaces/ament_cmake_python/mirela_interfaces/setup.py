1c48d16068bb3a87800e87b13ad0291e15d89797
samuellimabraz <samuellimabraz@gmail.com>
2025-09-09 15:04:07 -0300

    fix: Remove build and install directories from tracking and update .gitignore

--- build/mirela_interfaces/ament_cmake_python/mirela_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='mirela_interfaces',
-    version='0.0.0',
-    packages=find_packages(
-        include=('mirela_interfaces', 'mirela_interfaces.*')),
-)
--- install/mirela_interfaces/local/lib/python3.10/dist-packages/mirela_interfaces/msg/__init__.py
+++
@@ -1,3 +0,0 @@
-from mirela_interfaces.msg._aruco_transforms import ArucoTransforms  # noqa: F401
-from mirela_interfaces.msg._line_info import LineInfo  # noqa: F401
-from mirela_interfaces.msg._photo_info import PhotoInfo  # noqa: F401
