528eef82d7d5f849ec474919d82480de0702deb3
lipe-pedras <lfljp@hotmail.com>
2026-02-19 17:59:47 -0300

    feat: add brightness control and a publisher node for imx219

--- nectar/nectar/vision/nodes/__init__.py
+++ nectar/nectar/vision/nodes/__init__.py
@@ -1,6 +1,7 @@
 from .aruco_node import ArucoNode
 from .click_color_calibration_node import ClickColorCalibrationNode
 from .color_calibration_node import ColorCalibrationNode
+from .imx219_publisher_node import IMX219PublisherNode
 from .line_detection_node import LineDetectionNode
 from .webcam_publisher_node import WebcamPublisherNode
 
@@ -8,6 +9,7 @@ __all__ = [
     "ArucoNode",
     "ClickColorCalibrationNode",
     "ColorCalibrationNode",
+    "IMX219PublisherNode",
     "LineDetectionNode",
     "WebcamPublisherNode",
 ]
